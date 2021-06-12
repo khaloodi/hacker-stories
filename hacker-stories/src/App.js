fc44b6b5081247cc6a34d4e3a3b16373e18fd733
Khaled Adad <khaledadad@lewisu.edu>
2021-06-12 18:21:58 -0500

    moved list into a seperate module + rmvd idx.css

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import List from './List'
 
 const App = () => {
 
@@ -36,23 +37,6 @@ const App = () => {
       <List list = {stories}/>
     </div>
   );
-}
-
-const List = (props) => {
-  return(
-    <div>
-      {props.list.map(item =>
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      )}
-    </div>
-  )
 }
 
 export default App;
---
+++ hacker-stories/src/List.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const List = (props) => {
+  return(
+    <div>
+      {props.list.map(item =>
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default List
