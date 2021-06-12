13d47156d92d08843fa1f8d25f7d735fd82fce20
Khaled Adad <khaledadad@lewisu.edu>
2021-06-12 18:28:57 -0500

    added useState hook to display input val of search

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -4,26 +4,28 @@ import List from './List'
 const App = () => {
 
   const stories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
+  const [searchTerm, setSearchTerm] = React.useState('')
 
   const handleChange = (e) => {
-    console.log(e.target.value)
+    setSearchTerm(e.target.value)
   }
 
   return (
@@ -33,6 +35,10 @@ const App = () => {
       <input id='search' type='text' onChange={handleChange}/>
 
       <hr />
+
+      <p>
+        Seaching for: <strong>{searchTerm}</strong>
+      </p>
 
       <List list = {stories}/>
     </div>
