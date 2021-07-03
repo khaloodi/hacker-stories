14f332951e3b9bf12136924136320cb47314a993
Khaled Adad <khaledadad@lewisu.edu>
2021-07-03 12:16:58 -0500

    switch <> to <div> in Item changes align to stack

--- src/List.js
+++ src/List.js
@@ -39,14 +39,14 @@ const List = ({list}) => list.map(item => <Item key={item.objectID} item={item}/
 
 // concise syntac function w/o return statement
 const Item = ({item}) => (
-    <>
+    <div>
       <span>
         <a href={item.url}>{item.title}</a>
       </span>
       <span>{item.author}</span>
       <span>{item.num_comments}</span>
       <span>{item.points}</span>
-    </>
+    </div>
 )
 
 export default List
