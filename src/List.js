990a39f0f7f69a983d2f005c853125ae48e93ad1
Khaled Adad <khaledadad@lewisu.edu>
2021-07-03 12:14:34 -0500

    refactor step and obj destructuring in List.js

--- src/List.js
+++ src/List.js
@@ -1,20 +1,52 @@
 import React from 'react'
 
-const List = (props) => {
-  return(
-    <div>
-      {props.list.map(item =>
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      )}
-    </div>
-  )
-}
+// const List = (props) => {
+//   return(
+//     <div>
+//       {props.list.map(item =>
+//         <div key={item.objectID}>
+//           <span>
+//             <a href={item.url}>{item.title}</a>
+//           </span>
+//           <span>{item.author}</span>
+//           <span>{item.num_comments}</span>
+//           <span>{item.points}</span>
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+
+// const List = ({list}) => {
+//   return list.map(item => <Item key={item.objectID} item={item}/>)
+// }
+
+// concise syntac function w/o return statement
+const List = ({list}) => list.map(item => <Item key={item.objectID} item={item}/>)
+
+// const Item = ({item}) => {
+//   return (
+//     <>
+//       <span>
+//         <a href={item.url}>{item.title}</a>
+//       </span>
+//       <span>{item.author}</span>
+//       <span>{item.num_comments}</span>
+//       <span>{item.points}</span>
+//     </>
+//   )
+// }
+
+// concise syntac function w/o return statement
+const Item = ({item}) => (
+    <>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </>
+)
 
 export default List
